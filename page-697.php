5214c65c027f7d2538b35b11f84d709d1e44d7bf
Matt Cribbs <mcribbs@gmail.com>
2011-11-21 02:48:44 -0500

    Corrected article list display
    template names need to match WP call stack

---
+++ page-697.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Posts index
+*/
+get_header(); ?>
+		</div>
+		<!--header end-->		
+	</div>
+</div><div class="main">
+<!--content -->
+<div id="content">
+	<div class="col2">
+		<div class="wrapper">
+			<?php include (TEMPLATEPATH . '/theloop.php'); ?>
+
+		</div>
+	</div>
+	<div class="col1">
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+<!--content end-->
+<?php get_footer(); ?>
